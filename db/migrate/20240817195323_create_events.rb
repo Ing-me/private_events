f0ebdef101398d69e6a7c7edbb1f838f917b21d8
Djhymit Meide <djhymitmeide27@gmail.com>
2024-08-17 15:54:58 -0400

    Install devise, generate User model and Event model

---
+++ db/migrate/20240817195323_create_events.rb
@@ -0,0 +1,15 @@
+class CreateEvents < ActiveRecord::Migration[7.1]
+  def change
+    create_table :events do |t|
+      t.string :title
+      t.text :description
+      t.datetime :start_date
+      t.datetime :end_date
+      t.string :location
+      t.integer :capacity
+      t.string :status
+
+      t.timestamps
+    end
+  end
+end
